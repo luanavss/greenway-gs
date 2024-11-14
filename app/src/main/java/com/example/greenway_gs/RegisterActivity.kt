e87cbbc60bf74c224fb82d51fde559f747a03100
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-14 12:37:10 -0300

    :sparkles: feat: tela de cadastro e login estilizadas

---
+++ app/src/main/java/com/example/greenway_gs/RegisterActivity.kt
@@ -0,0 +1,33 @@
+package com.example.greenway_gs
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val buttonBackHome = findViewById<ImageButton>(R.id.backToHomeButton)
+
+        buttonBackHome.setOnClickListener{
+            var intent = Intent(this, MainActivity::class.java)
+
+            startActivity(intent)
+        }
+    }
+}
